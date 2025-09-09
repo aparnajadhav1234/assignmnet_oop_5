5b573a6e99a8718bf0ba0a3149ee7a76ca914dd1
aparnajadhav1234 <japarna300@gmail.com>
2025-09-09 17:01:26 +0530

    Add files via upload

---
+++ StringException.java
@@ -0,0 +1,16 @@
+public class StringException
+{
+public static void main(String args[])
+{
+String a=null;
+try{
+System.out.println(a.length());
+}
+catch(NullPointerException e )
+{
+System.out.println(e);
+
+}
+}
+
+}
