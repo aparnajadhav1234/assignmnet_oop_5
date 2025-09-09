d274be0ee72ef62c47e6824cce472094d011dca9
aparnajadhav1234 <japarna300@gmail.com>
2025-09-09 17:01:51 +0530

    Add files via upload

---
+++ ArrayException.java
@@ -0,0 +1,18 @@
+class ArrayException
+{
+public static void main(String args[])
+{
+ int arr[]={10,20,30,40,50};
+{
+try{
+System.out.println(arr[6]);
+}
+catch(Exception e)
+{
+System.out.println("Exception occure in 6 poisition");
+
+}
+System.out.println(arr[2]);
+}
+}
+}
